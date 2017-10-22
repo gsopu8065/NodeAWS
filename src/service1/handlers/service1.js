a0bcbfc03f4e65f91df0acfd71d98deb4e4b1732
jack <srujanjack@gmail.com>
2017-10-22 12:35:33 -0500

    updated package

--- src/service1/handlers/service1.js
+++ src/service1/handlers/service1.js
@@ -1,31 +1,5 @@
-var app = require('./../app');
-var aws = require('aws-sdk'),
-    multer = require('multer'),
-    multerS3 = require('multer-s3');
 
-aws.config.update({
-    secretAccessKey: 'XX',
-    accessKeyId: 'XX',
-    region: 'us-east-2'
-});
-
-var s3 = new aws.S3();
-
-var upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: 'sample8065',
-        metadata: function (req, file, cb) {
-            cb(null, {fieldName: file.fieldname});
-        },
-        key: function (req, file, cb) {
-            cb(null, file.originalname+Date.now().toString())
-        }
-    })
-});
-
-
-
+var app = require('myna-server').app;
 
 app.get('/hello', function (req, res) {
     res.send({ name:'Hello World Service'});
@@ -38,6 +12,7 @@ app.post('/go', function(req, res) {
 });
 
 //used by upload form
+/*
 app.post('/upload', upload.array('upl',1), function (req, res, next) {
     res.send("Uploaded!"+JSON.stringify(req.files));
-});
+});*/
